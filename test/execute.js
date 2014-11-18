a69b33170b3c510fcde263a70653a5cc03eed2f4
Gulthor <jbmusso+github@gmail.com>
2014-11-18 16:52:59 +0100

    Add a test checking for errors in execute() mode

--- test/execute.js
+++ test/execute.js
@@ -2,9 +2,13 @@
 var gremlin = require('../');
 
 describe('.execute()', function() {
-  it('should return a result and a response', function(done) {
-    var client = gremlin.createClient();
+  var client;
 
+  beforeEach(function() {
+    client = gremlin.createClient();
+  });
+
+  it('should return a result and a response', function(done) {
     client.execute('g.V()', function(err, result) {
       (err === null).should.be.true;
       result.length.should.equal(6);
@@ -13,16 +17,12 @@ describe('.execute()', function() {
   });
 
   it('should queue command before the client is connected', function(done) {
-    var client = gremlin.createClient();
-
     client.execute('g.V()', function() { });
     client.queue.length.should.equal(1);
     done();
   });
 
   it('should send commands after the client is connected', function(done) {
-    var client = gremlin.createClient();
-
     client.on('connect', function() {
       client.execute('g.V()', function(err, result) {
         (err === null).should.be.true;
@@ -33,8 +33,6 @@ describe('.execute()', function() {
   });
 
   it('should handle optional args', function(done) {
-    var client = gremlin.createClient();
-
     client.execute('g.v(1)', null, { args: { language: 'nashorn' }}, function(err, result) {
       (err === null).should.be.true;
       result.length.should.equal(1);
@@ -43,11 +41,19 @@ describe('.execute()', function() {
   });
 
   it('should handle bindings and optional args', function(done) {
-    var client = gremlin.createClient();
-
     client.execute('g.v(x)', { x: 1 }, { args: { language: 'nashorn' }}, function(err, result) {
       (err === null).should.be.true;
       result.length.should.equal(1);
+      done();
+    });
+  });
+
+  it('should handle errors', function(done) {
+    // pass a buggy script (missing parenthese)
+    var script = 'g.V(';
+
+    client.execute(script, function(err, result) {
+      (err === null).should.be.false;
       done();
     });
   });
