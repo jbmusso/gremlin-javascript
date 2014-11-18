d7a4d326792eb4abe855d140ca590d65cbd7fdb4
Gulthor <jbmusso+github@gmail.com>
2014-11-18 16:25:33 +0100

    Fix nashorn disambiguation test. Add simple raw test.

--- test/execute.nashorn.js
+++ test/execute.nashorn.js
@@ -2,10 +2,24 @@
 var gremlin = require('../');
 
 describe('.execute() # nashorn', function() {
-  it('should process raw JavaScript strings', function(done) {
-    var client = gremlin.createClient({ language: 'nashorn' });
+  var client;
 
-    var script = 'g.V()["filter(SPredicate)"](function(it) { return it.get().property("lang").orElse("") == "java" });';
+  beforeEach(function() {
+    client = gremlin.createClient({ language: 'nashorn' });
+  });
+
+  it('should process raw JavaScript strings', function(done) {
+    var script = 'g.V()';
+
+    client.execute(script, function(err, result) {
+      (err === null).should.be.true;
+      result.length.should.equal(6);
+      done();
+    });
+  });
+
+  it('should process raw JavaScript with nashorn disambiguation syntax', function(done) {
+    var script = 'g.V()["filter(Predicate)"](function(it) { return it.get().property("lang").orElse("") == "java" });';
 
     client.execute(script, function(err, result) {
       (err === null).should.be.true;
@@ -15,8 +29,6 @@ describe('.execute() # nashorn', function() {
   });
 
   it('should process JavaScript functions', function(done) {
-    var client = gremlin.createClient({ language: 'nashorn' });
-
     var script = function() {
       g.V();
     };
@@ -29,8 +41,6 @@ describe('.execute() # nashorn', function() {
   });
 
   it('should process inlined JavaScript functions', function(done) {
-    var client = gremlin.createClient({ language: 'nashorn' });
-
     client.execute(function() { g.V(); }, function(err, result) {
       (err === null).should.be.true;
       result.length.should.equal(6);
