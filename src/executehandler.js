205456294b0de0601f4db0b14ad6b365644b3324
jbmusso <jbmusso+github@gmail.com>
2015-05-06 18:47:13 +0200

    Move defaultExecuteHandler function to separate file

---
+++ src/executehandler.js
@@ -0,0 +1,32 @@
+'use strict';
+var highland = require('highland');
+var _ = {
+  isArray: require('lodash.isArray')
+};
+
+function defaultExecuteHandler(messageStream, callback) {
+  var errored = false;
+  var objectMode = false;
+
+  highland(messageStream)
+    .stopOnError(function(err) {
+      // TODO: this does not seem to halt the stream properly, and make
+      // the callback being fired twice. We need to get rid of the ugly
+      // errored variable check.
+      errored = true;
+      callback(err);
+    })
+    .map(function(message) {
+      objectMode = !_.isArray(message.result.data);
+
+      return message.result.data;
+    })
+    .sequence()
+    .toArray(function(results) {
+      if (!errored) {
+        callback(null, objectMode ? results[0] : results);
+      }
+    });
+}
+
+module.exports = defaultExecuteHandler;
--- src/gremlinclient.js
+++ src/gremlinclient.js
@@ -7,37 +7,14 @@ var inherits = require('util').inherits;
 var WebSocket = require('ws');
 var Guid = require('guid');
 var _ = {
-  defaults: require('lodash.defaults'),
-  isArray: require('lodash.isArray')
+  defaults: require('lodash.defaults')
 };
 var highland = require('highland');
 
 var MessageStream = require('./messagestream');
 
-function defaultExecuteHandler(messageStream, callback) {
-  var errored = false;
-  var objectMode = false;
 
-  highland(messageStream)
-    .stopOnError(function(err) {
-      // TODO: this does not seem to halt the stream properly, and make
-      // the callback being fired twice. We need to get rid of the ugly
-      // errored variable check.
-      errored = true;
-      callback(err);
-    })
-    .map(function(message) {
-      objectMode = !_.isArray(message.result.data);
-
-      return message.result.data;
-    })
-    .sequence()
-    .toArray(function(results) {
-      if (!errored) {
-        callback(null, objectMode ? results[0] : results);
-      }
-    });
-}
+var executeHandler = require('./executehandler');
 
 function GremlinClient(port, host, options) {
   this.port = port || 8182;
@@ -49,7 +26,7 @@ function GremlinClient(port, host, options) {
     op: 'eval',
     processor: '',
     accept: 'application/json',
-    executeHandler: defaultExecuteHandler //
+    executeHandler: executeHandler
   });
 
   this.useSession = this.options.session;
