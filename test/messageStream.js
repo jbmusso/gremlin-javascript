d342ebac2e87085f2da31eaa1f1c88e0049cdc6f
jbmusso <jbmusso+github@gmail.com>
2015-07-21 19:09:09 +0200

    Fix .messageStream() test was failing due to Gremlin protocol changes

--- test/messageStream.js
+++ test/messageStream.js
@@ -7,7 +7,7 @@ describe('.messageStream', function() {
     var s = client.messageStream('g.V()');
 
     s.on('data', function(message) {
-      message.status.code.should.eql(200);
+      message.status.code.should.be.within(200, 206);
       message.result.data.should.be.an('array');
     });
 
