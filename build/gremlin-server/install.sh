554f4e750ac3f1739aee0502c0395cd88526bcc6
jbmusso <jbmusso+github@gmail.com>
2017-06-03 01:39:37 +0200

    Add some logs

--- build/gremlin-server/install.sh
+++ build/gremlin-server/install.sh
@@ -8,12 +8,16 @@ export JRE_HOME=/usr/lib/jvm/java-8-oracle
 wget -O $HOME/apache-tinkerpop-gremlin-server-$GREMLIN_SERVER_VERSION-bin.zip "https://www.apache.org/dyn/closer.cgi?action=download&filename=tinkerpop/$GREMLIN_SERVER_VERSION/apache-tinkerpop-gremlin-server-$GREMLIN_SERVER_VERSION-bin.zip"
 unzip $HOME/apache-tinkerpop-gremlin-server-$GREMLIN_SERVER_VERSION-bin.zip -d $HOME/
 
+echo "Done extracting!"
+
+echo "Grabbing configuration file"
 # get gremlin-server configuration files
 cp ./build/gremlin-server/gremlin-server-js.yaml $HOME/apache-tinkerpop-gremlin-server-$GREMLIN_SERVER_VERSION/conf/
 
 # get neo4j dependencies
 # bin/gremlin-server.sh -i org.apache.tinkerpop neo4j-gremlin $GREMLIN_SERVER_VERSION
 
+echo "Starting gremlin-server..."
 # Start gremlin-server in the background and wait for it to be available
 cd $HOME/apache-tinkerpop-gremlin-server-$GREMLIN_SERVER_VERSION
 bin/gremlin-server.sh conf/gremlin-server-js.yaml > /dev/null 2>&1 &
