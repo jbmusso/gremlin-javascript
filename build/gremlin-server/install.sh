f27d8f43e7180f9e47df1d67f7d73f0684b10609
jbmusso <jbmusso+github@gmail.com>
2017-06-03 01:13:50 +0200

    No longer fetch incubating binaries

--- build/gremlin-server/install.sh
+++ build/gremlin-server/install.sh
@@ -5,17 +5,17 @@ export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 export JRE_HOME=/usr/lib/jvm/java-8-oracle
 
 # Install gremlin-server
-wget --no-check-certificate -O $HOME/apache-gremlin-server-$GREMLINSERVER_VERSION-incubating-bin.zip http://archive.apache.org/dist/incubator/tinkerpop/$GREMLINSERVER_VERSION-incubating/apache-gremlin-server-$GREMLINSERVER_VERSION-incubating-bin.zip
-unzip $HOME/apache-gremlin-server-$GREMLINSERVER_VERSION-incubating-bin.zip -d $HOME/
+wget --no-check-certificate -O $HOME/apache-tinkerpop-gremlin-server-$GREMLIN_SERVER_VERSION-bin.zip https://www.apache.org/dyn/closer.cgi?action=download&filename=tinkerpop/$GREMLIN_SERVER_VERSION/apache-tinkerpop-gremlin-server-$GREMLIN_SERVER_VERSION-bin.zip
+unzip $HOME/apache-tinkerpop-gremlin-server-$GREMLIN_SERVER_VERSION-bin.zip -d $HOME/
 
 # get gremlin-server configuration files
-cp ./build/gremlin-server/gremlin-server-js.yaml $HOME/apache-gremlin-server-$GREMLINSERVER_VERSION-incubating/conf/
+cp ./build/gremlin-server/gremlin-server-js.yaml $HOME/apache-tinkerpop-gremlin-server-$GREMLIN_SERVER_VERSION/conf/
 
 # get neo4j dependencies
-# bin/gremlin-server.sh -i org.apache.tinkerpop neo4j-gremlin $GREMLINSERVER_VERSION-incubating
+# bin/gremlin-server.sh -i org.apache.tinkerpop neo4j-gremlin $GREMLIN_SERVER_VERSION
 
 # Start gremlin-server in the background and wait for it to be available
-cd $HOME/apache-gremlin-server-$GREMLINSERVER_VERSION-incubating
+cd $HOME/apache-tinkerpop-gremlin-server-$GREMLIN_SERVER_VERSION
 bin/gremlin-server.sh conf/gremlin-server-js.yaml > /dev/null 2>&1 &
 cd $TRAVIS_BUILD_DIR
 sleep 30
