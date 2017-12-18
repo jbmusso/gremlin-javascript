8675c99c9cf720632e8bbaf9e32025e2bd55fd4f
Jacob Weyer <jacobweyer@gmail.com>
2017-12-18 09:35:52 -0500

    fixed todo related to properly halting the stream (#98)

--- gremlin-client/src/executeHandler.js
+++ gremlin-client/src/executeHandler.js
@@ -1,29 +1,10 @@
 import highland from 'highland';
-import _ from 'lodash';
 
-function defaultExecuteHandler(messageStream, callback) {
-  let errored = false;
-  let objectMode = false;
-
+const defaultExecuteHandler = (messageStream, callback) =>
   highland(messageStream)
-    .stopOnError(err => {
-      // TODO: this does not seem to halt the stream properly, and make
-      // the callback being fired twice. We need to get rid of the ugly
-      // errored variable check.
-      errored = true;
-      callback(err);
-    })
-    .map(({ result: { data } }) => {
-      objectMode = !_.isArray(data);
-
-      return data;
-    })
+    .map(({ result: { data } }) => data)
     .sequence()
-    .toArray(results => {
-      if (!errored) {
-        callback(null, objectMode ? results[0] : results);
-      }
-    });
-}
+    .collect()
+    .toCallback(callback);
 
 export default defaultExecuteHandler;
